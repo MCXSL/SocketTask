74357b0070651a9325f8f92cd0a9fb873fdb16ab
MCXSL <lo_ken@bk.ru>
2026-07-20 20:48:29 +0300

    Initial commit

---
+++ lib/library.cpp
@@ -0,0 +1,21 @@
+#include "library.h"
+
+#include <iostream>
+
+namespace lib
+{
+    void processString(std::string& str)
+    {
+        
+    }
+
+    int calculateSum(const std::string& str)
+    {
+        return 0;
+    }
+
+    bool checkSum(int sum)
+    {
+        return sum > 2 && sum % 32 == 0;
+    }
+}
---
+++ lib/library.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+namespace lib
+{
+    void processString(std::string& str);
+
+    int calculateSum(const std::string& str);
+
+    bool checkSum(int sum);
+}
